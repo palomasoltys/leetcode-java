8c5156adedb38bbda94cb4812f9cd3999dfceb8a
palomasoltys <pfs601@gmail.com>
2023-02-03 21:35:52 -0500

    Leetcode Medium

---
+++ src/PermutationInString.java
@@ -0,0 +1,34 @@
+public class PermutationInString {
+
+    public static void main(String[] args) {
+        System.out.println(checkInclusion("ab", "eidbaooo"));
+    }
+
+    static boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        if (n > m) return false;
+
+        int[] count = new int[26];
+        for (int i = 0; i < n; i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+        if (allZeros(count)) return true;
+
+        for (int i = n; i < m; i++) {
+            count[s2.charAt(i) - 'a']--;
+            count[s2.charAt(i - n) - 'a']++;
+            if (allZeros(count)) return true;
+        }
+
+        return false;
+    }
+
+    static boolean allZeros(int[] count) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
+    }
+}
